1c0b280b6c9514103753b33ce5b3fe8ffb40ac2a
jhilleri <jhi@live.fr>
2024-10-16 00:57:22 +0200

    Remove unused dependency in CreateCollectionUseCase.

--- todo.application/Collection/CreateCollectionUseCase.cs
+++ todo.application/Collection/CreateCollectionUseCase.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.Logging;
-using todo.application.core;
+﻿using todo.application.core;
 using todo.domain.Collection;
 using todo.domain.core;
 
@@ -8,10 +7,8 @@ namespace todo.application.Collection;
 public record CreateCollection(string Title) : IUseCase<Result<string>>;
 
 [Injectable]
-public class CreateCollectionUseCase(
-    ITaskCollectionRepository taskCollectionRepository,
-    ILogger<CreateCollectionUseCase> logger
-) : IUseCaseImplementation<CreateCollection, Result<string>>
+public class CreateCollectionUseCase(ITaskCollectionRepository taskCollectionRepository)
+    : IUseCaseImplementation<CreateCollection, Result<string>>
 {
     public async Task<Result<string>> Execute(CreateCollection request)
     {
